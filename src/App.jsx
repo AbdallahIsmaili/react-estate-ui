1f8ead31cf5ff5e75c72a60524409878f2914af2
AbdallahIsmaili <aismaili691@gmail.com>
2024-07-11 19:11:08 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Navbar from "./components/Navbar/Navbar"
 import "./layout.scss"
+import HomePage from "./routes/homePage/HomePage"
 
 function App() {
   return (
     <div className="layout">
       <Navbar />
+      <HomePage />
     </div>
   )
 }
